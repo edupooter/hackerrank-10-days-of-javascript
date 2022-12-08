a78f073eff5c385b2853f5a1988cb7574bc11d9b
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-08 18:57:44 -0300

    Added loop solution.

--- day2/vowels.js
+++ day2/vowels.js
@@ -3,5 +3,19 @@
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-  console.log('oi');
+  const knownVowels = 'aeiou'
+
+  function checkVowel(char) {
+    return knownVowels.indexOf(char) !== - 1
+  }
+
+  let vowels = []
+  let consonants = []
+
+  for (let char of s) {
+    checkVowel(char) ? vowels.push(char) : consonants.push(char)
+  }
+
+  vowels.forEach(element => console.log(element))
+  consonants.forEach(element => console.log(element))
 }
