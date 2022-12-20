4e53ba132cb10c601adb861d8a4c5d545ff33407
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-19 22:03:31 -0300

    Improved date function.

--- day6/dates.js
+++ day6/dates.js
@@ -1,12 +1,7 @@
 // The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 function getDayName(dateString) {
-  const dayNames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-  ];
-
-  const date = new Date(dateString);
-
-  const dayIndex = date.getDay();
-
-  return dayNames[dayIndex];
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const date = new Date(Date.parse(dateString));
+  const dayOfWeek = date.getDay();
+  return daysOfWeek[dayOfWeek];
 }
