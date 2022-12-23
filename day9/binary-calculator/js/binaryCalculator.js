5af9e800e3a5ab3357936b5e8596ccca55444cf7
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-23 18:32:43 -0300

    Implemented a very rudimentaty binary calculator.

--- day9/binary-calculator/js/binaryCalculator.js
+++ day9/binary-calculator/js/binaryCalculator.js
@@ -1,11 +1,49 @@
-function updateDisplay(value) {
-  document.getElementById('res').innerHTML = value;
-}
+const res = document.getElementById('res');
+const btn0 = document.getElementById('btn0');
+const btn1 = document.getElementById('btn1');
+const btnClr = document.getElementById('btnClr');
+const btnEql = document.getElementById('btnEql');
+const btnSum = document.getElementById('btnSum');
+const btnSub = document.getElementById('btnSub');
+const btnMul = document.getElementById('btnMul');
+const btnDiv = document.getElementById('btnDiv');
 
-const add = (a, b) => a + b;
+let currentVal = '';
 
-const subtract = (a, b) => a - b;
+const addToCurrentVal = (val) => {
+  currentVal += val;
+  res.innerHTML = currentVal;
+};
 
-const multiply = (a, b) => a * b;
+const clearCurrentVal = () => {
+  currentVal = '';
+  res.innerHTML = currentVal;
+};
 
-const divide = (a, b) => Math.floor(a / b);
+const performOperation = () => {
+  const regex = /([01]+)([\+|\-|\*|\/])([01]+)/;
+  const result = currentVal.match(regex);
+  const a = parseInt(result[1], 2);
+  const b = parseInt(result[3], 2);
+  let operationResult;
+
+  switch (result[2]) {
+    case '+':
+      operationResult = a + b;
+      break;
+    case '-':
+      operationResult = a - b;
+      break;
+    case '*':
+      operationResult = a * b;
+      break;
+    case '/':
+      operationResult = Math.floor(a / b);
+      break;
+  }
+
+  res.innerHTML = operationResult.toString(2);
+};
+
+btn0.addEventListener('click', () => addToCurrentVal('0'));
+btn1.addEventListener('click', () => addToCurrentVal('1'));
