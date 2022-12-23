37f8b11f2687eb2bd15e55a3723fc81a3867631a
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-23 16:59:18 -0300

    Fixed last commit.

--- day8/buttons-container/js/buttonsGrid.js
+++ day8/buttons-container/js/buttonsGrid.js
@@ -15,6 +15,16 @@ function rotateButtons() {
   const currentState = clickCount % 7;
 
   switch (currentState) {
+    case 0:
+      btn1.innerHTML = 1;
+      btn2.innerHTML = 2;
+      btn3.innerHTML = 3;
+      btn4.innerHTML = 4;
+      btn6.innerHTML = 6;
+      btn7.innerHTML = 7;
+      btn8.innerHTML = 8;
+      btn9.innerHTML = 9;
+      break;
     case 1:
       btn1.innerHTML = 4;
       btn2.innerHTML = 1;
