c2a4e815b0e9ebe414b15d2431f9369aa9d5df80
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-23 18:10:54 -0300

    Added binary calculator.

---
+++ day9/binary-calculator/js/binaryCalculator.js
@@ -0,0 +1,11 @@
+function updateDisplay(value) {
+  document.getElementById('res').innerHTML = value;
+}
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => Math.floor(a / b);
