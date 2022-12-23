09fabb3c2caa6cf5362f76428059583e3de0a99b
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-12-23 16:38:31 -0300

    Fixed initial state.

--- day8/buttons-container/js/buttonsGrid.js
+++ day8/buttons-container/js/buttonsGrid.js
@@ -15,16 +15,6 @@ function rotateButtons() {
   const currentState = clickCount % 7;
 
   switch (currentState) {
-    case 0:
-      btn1.innerHTML = 1;
-      btn2.innerHTML = 2;
-      btn3.innerHTML = 3;
-      btn4.innerHTML = 4;
-      btn6.innerHTML = 6;
-      btn7.innerHTML = 7;
-      btn8.innerHTML = 8;
-      btn9.innerHTML = 9;
-      break;
     case 1:
       btn1.innerHTML = 4;
       btn2.innerHTML = 1;
@@ -84,6 +74,16 @@ function rotateButtons() {
       btn7.innerHTML = 1;
       btn8.innerHTML = 4;
       btn9.innerHTML = 7;
+      break;
+    case 7:
+      btn1.innerHTML = 2;
+      btn2.innerHTML = 3;
+      btn3.innerHTML = 6;
+      btn4.innerHTML = 1;
+      btn6.innerHTML = 9;
+      btn7.innerHTML = 4;
+      btn8.innerHTML = 7;
+      btn9.innerHTML = 8;
       break;
   }
 }
